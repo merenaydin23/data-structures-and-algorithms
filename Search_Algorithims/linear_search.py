329b44d07eed259fed8092114e8809f037a284eb
Muhammed Eren <147327203+merenaydin23@users.noreply.github.com>
2025-10-19 14:28:56 +0300

    Linear Search
    Linear search checks each item in a list one by one.
    
    Best case: Item is first → O(1)
    
    Worst case: Item is last or not present → O(n)
    
    Average case: Item is somewhere in the middle → O(n)

---
+++ Search_Algorithims/linear_search.py
@@ -0,0 +1,21 @@
+def linear_search(array, x):
+    i = 0
+    while i < len(array):
+        if array[i] == x:
+            print("Element found at index", i)
+            return
+        i += 1
+    print("Element not found in the array")
+    return
+
+def MAIN():
+    array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    x = int(input("Please enter the number to search: "))
+    linear_search(array, x)
+
+MAIN()
+
+# Linear Search Time Complexity:
+# Best case: O(1) → element is first
+# Worst case: O(n) → element is last or not found
+# Average case: O(n/2) → element is somewhere in the middle
