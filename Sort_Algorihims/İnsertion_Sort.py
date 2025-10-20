cddcbb310a03391f2e00f412ebfc435d48ec4165
Muhammed Eren <147327203+merenaydin23@users.noreply.github.com>
2025-10-20 18:51:40 +0300

    Insertion Sort Algorithms in Python
    Two versions of Insertion Sort in Python. .

---
+++ Sort_Algorihims/İnsertion_Sort.py
@@ -0,0 +1,42 @@
+def insertion_sort_1(arr):
+    step = 0
+    i = 0
+    j = 1
+    while step < len(arr):
+        step = step + 1
+        a = i
+        b = j
+        while b > 0 and arr[a] > arr[b]:
+            temp = arr[a]
+            arr[a] = arr[b]
+            arr[b] = temp
+            b = b - 1
+            a = a - 1
+
+        i = i + 1
+        j = j + 1
+        if j == len(arr):
+            return arr
+# Time complexity: Best case O(n), Worst case O(n^2), Average case O(n^2)
+
+
+def insertion_sort_2(A):
+    n = len(A)
+    for i in range(1, n):
+        j = i - 1
+        key = A[i]
+        while j >= 0 and A[j] > key:
+            A[j + 1] = A[j]
+            j -= 1
+        A[j + 1] = key
+    return A
+# Time complexity: Best case O(n), Worst case O(n^2), Average case O(n^2)
+
+def Main():
+    print("Insertion Sort")
+    a = [2, 22, 0, 55, 11, 33, 20, 0, 77, -9, 888, 77]
+    b = insertion_sort_1(a)
+    print(b)
+    c = insertion_sort_2(b)
+    print(c)
+Main()
